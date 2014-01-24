c4ec1f42158952826f09f454dfe8025c5f305cd7
Trung Ngo <ndtrung4419@gmail.com>
2014-01-24 18:40:12 +0700

    Make (un)registration code classmethods and called by comtypes
    Previously, I have to manually patch py2exe's boot_ctypes_com_server.py
    file to call the registration functions on DllRegisterServer and
    DllUnregisterServer.

--- bogo_server.py
+++ bogo_server.py
@@ -13,6 +13,7 @@ from comtypes.gen.TSF import ITfInputProcessorProfiles, ITfCategoryMgr
 CLSID_TF_InputProcessorProfiles = "{33C53A50-F456-4884-B049-85FD643ECFED}"
 CLSID_TF_CategoryMgr = "{A4B544A1-438D-4B41-9325-869523E2D6C7}"
 GUID_TFCAT_TIP_KEYBOARD = GUID("{34745C63-B2F0-4784-8B67-5E12C8701A31}")
+serverGUIDPointer = ctypes.pointer(GUID("{4581A23E-03EA-4614-975B-FF6206A8B840}"))
 
 
 class BoGoTextService(BoGo):
@@ -24,51 +25,67 @@ class BoGoTextService(BoGo):
     _reg_clsctx_ = comtypes.CLSCTX_INPROC_SERVER
     # _reg_typelib_ = "interfaces\\bogo.tlb"
 
+    # The _register and _unregister class methods are called by comtypes.server.register
+    # as custom hooks for registration and unregistration.
+
+    @classmethod
+    def _register(self, registrar):
+        registrar._unregister(BoGoTextService, force=True)
+        registrar._register(BoGoTextService)
+
+        # Register input profile (supported languages, description,...)
+        inputProcessorProfiles = comtypes.client.CreateObject(CLSID_TF_InputProcessorProfiles,
+                                    clsctx=comtypes.CLSCTX_INPROC_SERVER,
+                                    interface=ITfInputProcessorProfiles)
+
+        profileGUIDPointer = serverGUIDPointer
+
+        description = ctypes.c_wchar_p("BoGo")
+        description = ctypes.cast(description, ctypes.POINTER(ctypes.c_ushort))
+
+        # http://msdn.microsoft.com/en-us/library/windows/desktop/dd318693%28v=vs.85%29.aspx
+        VI_VN = 0x042A
+
+        hr = inputProcessorProfiles.Register(serverGUIDPointer)
+
+        print(hr)
+
+        hr = inputProcessorProfiles.AddLanguageProfile(
+            serverGUIDPointer,
+            VI_VN,
+            profileGUIDPointer,
+            description,
+            4,                   # The description is 4-char long
+            None,                # We don't have icons
+            -1,
+            -1)
+
+        print(hr)
+
+        # Register categories (whether we do keyboard, voice,...)
+        categoryManager = comtypes.client.CreateObject(CLSID_TF_CategoryMgr,
+            clsctx=comtypes.CLSCTX_INPROC_SERVER,
+            interface=ITfCategoryMgr)
+
+        hr = categoryManager.RegisterCategory(serverGUIDPointer,
+            ctypes.pointer(GUID_TFCAT_TIP_KEYBOARD),
+            serverGUIDPointer)
+
+        print(hr)
+
+    @classmethod
+    def _unregister(self, registrar):
+        inputProcessorProfiles = comtypes.client.CreateObject(CLSID_TF_InputProcessorProfiles,
+                                    clsctx=comtypes.CLSCTX_INPROC_SERVER,
+                                    interface=ITfInputProcessorProfiles)
+
+        hr = inputProcessorProfiles.Unregister(serverGUIDPointer)
+
+        registrar._unregister(BoGoTextService)
+
     def Activate(self, thread_manager, client_id):
         pass
 
     def Deactivate(self):
         pass
 
-
-def on_registered():
-    # Register input profile (language, description,...)
-    inputProcessorProfiles = comtypes.client.CreateObject(CLSID_TF_InputProcessorProfiles,
-                                clsctx=comtypes.CLSCTX_INPROC_SERVER,
-                                interface=ITfInputProcessorProfiles)
-
-    serverGUIDPointer = ctypes.pointer(GUID("{4581A23E-03EA-4614-975B-FF6206A8B840}"))
-    profileGUIDPointer = serverGUIDPointer
-
-    description = ctypes.c_wchar_p("BoGo")
-    description = ctypes.cast(description, ctypes.POINTER(ctypes.c_ushort))
-
-    # http://msdn.microsoft.com/en-us/library/windows/desktop/dd318693%28v=vs.85%29.aspx
-    VI_VN = 0x042A
-    
-    hr = inputProcessorProfiles.Register(serverGUIDPointer)
-
-    print(hr)
-
-    hr = inputProcessorProfiles.AddLanguageProfile(
-        serverGUIDPointer,
-        VI_VN,
-        profileGUIDPointer,
-        description,
-        4,
-        None,
-        -1,
-        -1)
-
-    print(hr)
-
-    # Register categories (whether we do keyboard, voice,...)
-    categoryManager = comtypes.client.CreateObject(CLSID_TF_CategoryMgr,
-        clsctx=comtypes.CLSCTX_INPROC_SERVER,
-        interface=ITfCategoryMgr)
-
-    hr = categoryManager.RegisterCategory(serverGUIDPointer,
-        ctypes.pointer(GUID_TFCAT_TIP_KEYBOARD),
-        serverGUIDPointer)
-
-    print(hr)
