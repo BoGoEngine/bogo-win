db9ce619aea1f981dded2bd8f1f08823af21da85
Trung Ngo <ndtrung4419@gmail.com>
2014-01-27 22:47:28 +0700

    Better setup.py script
    - Change output to bogo.dll
    - Don't build EXE file
    - exclide w9xpopen.exe
    - Add maintainer's info

--- setup.py
+++ setup.py
@@ -4,10 +4,15 @@ from distutils.core import setup
 BoGo = dict(
     modules = ["bogo_server"],
     other_resources = [("TYPELIB", 1, file(r"interfaces\\bogo.tlb", "rb").read())],
-    dest_base = r"testComServer"
+    dest_base = r"bogo",
+    create_exe = False
     )
 
 setup(name="BoGo",
+      version="0.1.0",
+      author="Trung Ngo",
+      author_email="ndtrung4419@gmail.com",
       ctypes_com_server = [BoGo],
       zipfile=None,
+      options={"py2exe": {"bundle_files": 3, "dll_excludes": ["w9xpopen.exe"]}},
       )
